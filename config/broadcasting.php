15ac808f807adb4071e31b53b4c9e698ee630aea
Rohan Swami <rohan@acruxsoftwares.com>
2024-03-31 17:07:01 +0530

    broadcasting default driver reverb set

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -13,9 +13,9 @@ return [
     |
     | Supported: "reverb", "pusher", "ably", "redis", "log", "null"
     |
-    */
+     */
 
-    'default' => env('BROADCAST_CONNECTION', 'null'),
+    'default' => env('BROADCAST_CONNECTION', 'reverb'),
 
     /*
     |--------------------------------------------------------------------------
@@ -26,7 +26,7 @@ return [
     | to broadcast events to other systems or over WebSockets. Samples of
     | each available type of connection are provided inside this array.
     |
-    */
+     */
 
     'connections' => [
 
@@ -53,7 +53,7 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
+                'host' => env('PUSHER_HOST') ?: 'api-' . env('PUSHER_APP_CLUSTER', 'mt1') . '.pusher.com',
                 'port' => env('PUSHER_PORT', 443),
                 'scheme' => env('PUSHER_SCHEME', 'https'),
                 'encrypted' => true,
