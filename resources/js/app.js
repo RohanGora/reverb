85d711973dc2762e67be6966b123c04b57fcb526
Rohan Swami <rohan@acruxsoftwares.com>
2024-03-31 18:29:14 +0530

    chat page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
-import './bootstrap';
+import "./bootstrap";
+import { createApp } from "vue";
+import chats from "./chats.vue";
+
+const app = createApp({});
+app.component("chats", chats);
+app.mount("#app");
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\ChatController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('', 'welcome');
+Route::view('chats', 'chats');
+Route::get('get-messages', [ChatController::class, 'getMessages']);
+Route::post('store-messages', [ChatController::class, 'storeMessages']);
