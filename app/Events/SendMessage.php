70456edd113f499defbd5041c68ed4d1e569cbb1
Rohan Swami <rohan@acruxsoftwares.com>
2024-03-31 21:11:08 +0530

    typo

--- app/Events/SendMessage.php
+++ app/Events/SendMessage.php
@@ -5,11 +5,11 @@ namespace App\Events;
 use App\Models\Chat;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class SendMessage implements ShouldBroadcast
+class SendMessage implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
     public $message;
